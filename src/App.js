ea527f3db6f0cfd4ab325593086dabd2ca6e9433
Erhan Arda <erhanarda7@gmail.com>
2020-04-18 15:02:22 +0300

    ternary and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0,
+    }
+  }
+/*
+  decrement = props => {
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
+  increment = props => {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+*/
+
+//Ternary 
+  change = (operation) => {
+    this.setState({
+      counter: operation === "decrement" ? this.state.counter - 1 : this.state.counter + 1
+    })
+  }
+
+
+
+
+  render() {
+    const { counter } = this.state
+    return <div>
+      <h1>{counter}</h1>
+      <button onClick={() => {this.change("decrement")}}>-</button>
+      <button onClick={()=>{this.change("increment")}}>+</button>
     </div>
-  );
+  }
 }
 
 export default App;
